f6a2d87cebc99159d2722eee8fc18286d240e2dd
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2022-06-28 19:57:59 +0300

    implement Login compenent with google auot and save the user in Sanity

--- share-me-frontend/src/App.js
+++ share-me-frontend/src/App.js
@@ -1,9 +1,21 @@
+import { useEffect } from "react";
+import { gapi } from "gapi-script";
 import { Routes, Route, useNavigate } from "react-router-dom";
 
 import Login from "./components/Login";
 import Home from "./containers/Home";
 
 const App = () => {
+  useEffect(() => {
+    const start = () => {
+      gapi.client.init({
+        clientId: process.env.REACT_APP_GOOGLE_API_TOKEN,
+        scope: "",
+      });
+    };
+    gapi.load("client:auth2", start);
+  });
+
   return (
     <Routes>
       <Route path="login" element={<Login />} />
